c6108dc49b81b7e68fa9860badb46d0385168493
Casey Slaught <caseyslaught@gmail.com>
2023-03-27 15:18:01 -0400

    added email

--- notebooks/startJupyter.bat
+++ notebooks/startJupyter.bat
@@ -1,2 +1,2 @@
 cd C:\Users\casey\Work\SmartCarteContainers\notebooks
-docker run -p 8888:8888 -v %cd%/src:/home/src -v %cd%/data:/home/src/data -e LABELBOX_API_KEY=%SMARTCARTE_LABELBOX_API_KEY% sc_notebook:latest
+docker run -p 8888:8888 -v %cd%/src:/home/src -v %cd%/data:/home/src/data --env-file .env -e LABELBOX_API_KEY=%SMARTCARTE_LABELBOX_API_KEY% sc_notebook:latest
